3747d28f83270884b41bd0f804427fc8b395b86c
YAKUBUUU26 <167569319+YAKUBUUU26@users.noreply.github.com>
2024-12-13 00:51:56 +0300

    Update encrypt_client.py

--- encrypt_client.py
+++ encrypt_client.py
@@ -5,11 +5,7 @@ key = 'IJ-lCg5DE59yIRX4rfvuaXb6HGwOWGtGlxeWFMYyroQ='
 cipher_suite = Fernet(key)
 
 # Encrypt the message
-message = ("English texts for beginners to practice reading and comprehension "
-           "online and for free. Practicing your comprehension of written English "
-           "will both improve your vocabulary and understanding of grammar "
-           "and word order. The texts below are designed to help you develop"
-           " while giving you an instant evaluation of your progress.")
+message = ("English texts for beginners to practice reading and comprehension ")
 encrypted_message = cipher_suite.encrypt(message.encode())
 
 # Print the encrypted message in bytes
